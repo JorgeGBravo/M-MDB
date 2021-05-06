2fb73c878063dda0d725c855cd2798553755570b
Jorge González Bravo <JorgeGBravo@users.noreply.github.com>
2021-05-06 11:22:53 +0100

    Controlado inserccion de array objetos en campos searchQuery den tabla data linea 39 -DataController-

---
+++ app/Models/Searchquery.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class searchquery extends Model
+{
+    protected $table = "searchqueries";
+
+    protected $primaryKey = "idQueries";
+}
